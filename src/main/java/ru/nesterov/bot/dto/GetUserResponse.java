d2a638a1c49cd01916993936ccc2a7bafcd6e933
alexander <nesterov1441@gmail.com>
2026-02-07 21:01:43 +0300

    initial commit

--- src/main/java/ru/nesterov/bot/dto/GetUserResponse.java
+++ src/main/java/ru/nesterov/bot/dto/GetUserResponse.java
@@ -1,10 +1,14 @@
 package ru.nesterov.bot.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class GetUserResponse {
     private long userId;
     private String username;
