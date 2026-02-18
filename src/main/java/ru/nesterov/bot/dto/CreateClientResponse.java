de307ea0267f1ea9ee4b7590c23c6b0295aaa6c0
alexander <nesterov1441@gmail.com>
2026-02-18 15:08:12 +0300

    fix

--- src/main/java/ru/nesterov/bot/dto/CreateClientResponse.java
+++ src/main/java/ru/nesterov/bot/dto/CreateClientResponse.java
@@ -1,12 +1,17 @@
 package ru.nesterov.bot.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.autoconfigure.AutoConfigurationPackage;
 
 import java.util.Date;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CreateClientResponse {
     private long id;
     private String name;
