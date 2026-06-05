383e1875cbc68f5736800f316b799604913502db
alexander <nesterov1441@gmail.com>
2026-06-05 12:23:31 +0300

    some fixes

--- src/main/java/ru/nesterov/bot/KeyboardUpdateService.java
+++ src/main/java/ru/nesterov/bot/KeyboardUpdateService.java
@@ -26,12 +26,7 @@ public class KeyboardUpdateService {
 
     @NotNull
     public List<BotApiMethod<?>> getUpdateKeyboard(Update update) {
-        String messageText = update.hasMessage() ? update.getMessage().getText() : null;
         if (!shouldUpdateKeyboard(TelegramUpdateUtils.getChatId(update))) {
-            return List.of();
-        }
-        if ("/start".equals(messageText)) {
-            shouldUpdateKeyboard(TelegramUpdateUtils.getChatId(update));
             return List.of();
         }
         return updateUserControlButtonsHandler.handle(update);
--- src/main/java/ru/nesterov/bot/handlers/implementation/invocable/GetUnpaidEventsHandler.java
+++ src/main/java/ru/nesterov/bot/handlers/implementation/invocable/GetUnpaidEventsHandler.java
@@ -43,6 +43,7 @@ public class GetUnpaidEventsHandler extends InvocableCommandHandler {
         }
         return message.toString();
     }
+
     @Override
     public int getOrder() {
         return 2;
