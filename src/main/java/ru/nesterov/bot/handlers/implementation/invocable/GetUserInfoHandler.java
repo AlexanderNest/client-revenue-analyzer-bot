1455da9960950d386fb680cf26e7116c44c97cff
PavelLyalko <demon00222@gmail.com>
2026-06-19 11:22:56 +0300

    добавил обработчик получить-информацию-о-пользователе -#159

---
+++ src/main/java/ru/nesterov/bot/handlers/implementation/invocable/GetUserInfoHandler.java
@@ -0,0 +1,48 @@
+package ru.nesterov.bot.handlers.implementation.invocable;
+
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import ru.nesterov.bot.dto.GetUserRequest;
+import ru.nesterov.bot.dto.GetUserResponse;
+import ru.nesterov.bot.handlers.abstractions.InvocableCommandHandler;
+import ru.nesterov.bot.utils.TelegramUpdateUtils;
+
+import java.util.List;
+
+@Component
+public class GetUserInfoHandler extends InvocableCommandHandler {
+
+    @Override
+    public String getCommand() {
+        return "Информация о пользователе";
+    }
+
+    @Override
+    public List<BotApiMethod<?>> handle(Update update) {
+        long chatId = TelegramUpdateUtils.getChatId(update);
+        long telegramUserId = TelegramUpdateUtils.getUserId(update);
+
+        GetUserRequest request = new GetUserRequest();
+        request.setUsername(String.valueOf(telegramUserId));
+        GetUserResponse user = client.getUserByUsername(request);
+
+        if (user == null) {
+            return getPlainSendMessage(chatId, "Пользователь не найден в системе.");
+        }
+
+        String message = String.format(
+                "Информация о пользователе:%n" +
+                "Telegram ID: %d%n" +
+                "ID в системе: %d%n" +
+                "Username: %s%n" +
+                "Роль: %s",
+                telegramUserId,
+                user.getUserId(),
+                user.getUsername(),
+                user.getRole()
+        );
+
+        return getPlainSendMessage(chatId, message);
+    }
+}
