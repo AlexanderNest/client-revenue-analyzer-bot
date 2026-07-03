cf68ab5af0f1753b9b57fad64daecebc61a2df74
PavelLyalko <demon00222@gmail.com>
2026-07-03 22:23:17 +0300

    начал задачу 311

--- src/main/java/ru/nesterov/bot/handlers/implementation/invocable/stateful/GetPdfPeport/GetReportHandler.java
+++ src/main/java/ru/nesterov/bot/handlers/implementation/invocable/stateful/GetPdfPeport/GetReportHandler.java
@@ -3,6 +3,7 @@ package ru.nesterov.bot.handlers.implementation.invocable.stateful.GetPdfPeport;
 import org.springframework.stereotype.Component;
 import ru.nesterov.bot.handlers.abstractions.StatefulCommandHandler;
 import ru.nesterov.bot.handlers.implementation.invocable.stateful.getSchedule.InlineCalendarBuilder;
+import ru.nesterov.bot.statemachine.dto.Action;
 
 @Component
 public class GetReportHandler extends StatefulCommandHandler<State, GetPdfReportRequest> {
@@ -16,7 +17,20 @@ public class GetReportHandler extends StatefulCommandHandler<State, GetPdfReport
 
     @Override
     protected void initTransitions() {
+        stateMachineProvider
+                .addTransition(State.STARTED, Action.COMMAND_INPUT, State.SELECT_CLIENT, this::handleCommandInputAndSendClients)
 
+                .addTransition(State.SELECT_CLIENT, Action.ANY_CALLBACK_INPUT, State.SELECT_FIRST_DATE, this::handleClientName)
+
+                .addTransition(State.SELECT_FIRST_DATE, Action.CALLBACK_DATE, State.SELECT_SECOND_DATE, this::handleFirstDate)
+                .addTransition(State.SELECT_FIRST_DATE, Action.CALLBACK_PREV, State.SELECT_FIRST_DATE, this::handleCallbackPrev)
+                .addTransition(State.SELECT_FIRST_DATE, Action.CALLBACK_NEXT, State.SELECT_FIRST_DATE, this::handleCallbackNext)
+                .addTransition(State.SELECT_FIRST_DATE, Action.CALLBACK_TODAY, State.SELECT_FIRST_DATE, this::handleCallbackToday)
+
+                .addTransition(State.SELECT_SECOND_DATE, Action.CALLBACK_DATE, State.FINISH, this::handleSecondDate)
+                .addTransition(State.SELECT_SECOND_DATE, Action.CALLBACK_PREV, State.SELECT_SECOND_DATE, this::handleCallbackPrev)
+                .addTransition(State.SELECT_SECOND_DATE, Action.CALLBACK_NEXT, State.SELECT_SECOND_DATE, this::handleCallbackNext)
+                .addTransition(State.SELECT_SECOND_DATE, Action.CALLBACK_TODAY, State.SELECT_SECOND_DATE, this::handleCallbackToday);
     }
 
     @Override
